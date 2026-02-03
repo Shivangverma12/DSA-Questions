0a1d80d32dd0fe8d7dfd3f2dfc5985d67b9290f8
Shivang Verma <shivangverma7355@gmail.com>
2026-02-03 22:32:32 +0530

    updated for four values.

--- average.java
+++ average.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 
 public class average
 {
-    public static void findaverage(int num1,int num2,int num3)
+    public static void findaverage(int num1,int num2,int num3,int num4)
     {
-        int findaveragevalue=(num1+num2+num3)/3;
+        int findaveragevalue=(num1+num2+num3+num4)/4;
         System.out.println(findaveragevalue);
         return;
     }
@@ -13,7 +13,8 @@ public class average
         int num1=sc.nextInt();
         int num2=sc.nextInt();
         int num3=sc.nextInt();
-        findaverage(num1, num2, num3);
+        int num4=sc.nextInt();
+        findaverage(num1, num2, num3, num4);
     }
 
 }
